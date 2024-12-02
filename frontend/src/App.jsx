290e6f6241f4b0450d5bbee5102183eedadbc5c4
akash jha <akashjhasciencea@gmail.com>
2024-12-02 18:16:37 +0530

    Images

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,7 @@ import Addiction from './pages/Addiction';
 import MentalHealth from './pages/MentalHealth';
 import Meditations from './pages/Meditations';
 import Ai from './components/Ai';
-import Support from './pages/Support'
+import Hopecore from './pages/Hopecore'
 import ChatsPage from './pages/ChatsPage';
 const App = () => {
   return (
@@ -18,7 +18,7 @@ const App = () => {
         <Route path="/MentalHealth" element={<MentalHealth/>} />
         <Route path="/Addiction" element={<Addiction />} />
         <Route path="/meditations" element={<Meditations />} />
-        <Route path='/Support' element={<Support/>}/>
+        <Route path='/Hopecore' element={<Hopecore/>}/>
         <Route path='/chats' element={<ChatsPage/>}/>
       </Routes>
       <Ai/>
--- frontend/src/pages/ChatsPage.jsx
+++ frontend/src/pages/ChatsPage.jsx
@@ -1,65 +1,74 @@
-import { auth } from './firebase';   
-import { useState, useEffect } from 'react';   
-import { GoogleAuthProvider, signInWithPopup,signOut, setPersistence, browserLocalPersistence, onAuthStateChanged } from 'firebase/auth';  
-import Chat from '../components/Chat';  
-const provider = new GoogleAuthProvider();  
-
-const ChatsPage = () => {  
-    const [user, setUser] = useState(null);  
-
-    useEffect(() => {  
-        const setAuthPersistence = async () => {  
-            try {  
-                await setPersistence(auth, browserLocalPersistence);    
-            } catch (error) {  
-                console.error('Error setting persistence:', error);  
-            }  
-            setTimeout(()=>{
-                document.getElementById('chatbase-bubble-button').style.display='none';
-                document.getElementById('ai-fix').style.display='none';
-            },1000)
-        };  
-
-        setAuthPersistence();  
-        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {  
-            if (currentUser) {  
-                setUser(currentUser.displayName);  
-            } else {  
-                setUser(null);  
-            }  
-        });  
-        return () => unsubscribe();  
-    }, []);  
-    const handleLogout = async () => {  
-        try {  
-            await signOut(auth);  
-            setUser(null);  
-            console.log('User signed out.');  
-        } catch (error) {  
-            console.error('Error signing out: ', error);  
-        }  
+import { auth } from "./firebase";
+import { useState, useEffect } from "react";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  setPersistence,
+  browserLocalPersistence,
+  onAuthStateChanged,
+} from "firebase/auth";
+import Chat from "../components/Chat";
+const provider = new GoogleAuthProvider();
+const ChatsPage = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const setAuthPersistence = async () => {
+      try {
+        await setPersistence(auth, browserLocalPersistence);
+      } catch (error) {
+        console.error("Error setting persistence:", error);
+      }
+      setTimeout(() => {
+        document.getElementById("chatbase-bubble-button").style.display =
+          "none";
+        document.getElementById("ai-fix").style.display = "none";
+      }, 1000);
     };
+    setAuthPersistence();
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser.displayName);
+      } else {
+        setUser(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      setUser(null);
+      console.log("User signed out.");
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
+  };
 
-    const handleLogin = async () => {  
-        try {  
-            const result = await signInWithPopup(auth, provider);  
-            setUser(result.user.displayName);  
-        } catch (error) {  
-            console.error("Error signing in: ", error);  
-        }  
+  const handleLogin = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      setUser(result.user.displayName);
+    } catch (error) {
+      console.error("Error signing in: ", error);
+    }
+  };
 
-    };  
-    
-
-    return (  
-        <div>  
-            {user ? <><button onClick={handleLogout}>Signout</button> <Chat user={user} /></> :   
-                <div className='chatsp'>  
-                    <button className='books-but' onClick={handleLogin}>Continue with Google</button>  
-                </div>  
-            }  
-        </div>  
-    );  
-};  
+  return (
+    <div>
+      {user ? (
+        <>
+          <button onClick={handleLogout}>Signout</button> <Chat user={user} />
+        </>
+      ) : (
+        <div className="chatsp">
+          <button className="books-but" onClick={handleLogin}>
+            Continue with Google
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default ChatsPage;
