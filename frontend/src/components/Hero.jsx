1deeda3d9079b37bd1d4f5839727c9d1b2328750
akash jha <akashjhasciencea@gmail.com>
2025-01-05 22:08:37 +0530

    final

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,7 +3,7 @@ import {gsap} from 'gsap'
 import { useGSAP } from '@gsap/react'
 function Hero() {
   useGSAP(()=>{
-    gsap.from('#hero',{opacity:0,x:-60,delay:0.2,duration:0.4})
+    gsap.from('#hero',{opacity:0,x:-60,delay:0,duration:0.4})
   },[])
   return (
     <div id='hero'>
